5b862f8387f8247457532cfa6593cdbf43619eb0
Ivan Ramiro Jimenez <irjimenez@smdigital.com.co>
2019-07-16 15:47:53 -0500

    blog and custom body classes

--- footer.php
+++ footer.php
@@ -3,5 +3,7 @@
 <footer>
     <p>This is my footer</p>
 </footer>
+<?php wp_nav_menu(array('theme_location'=>'footer')); ?>
 </body>
+<?php wp_footer(); ?>
 </html>
--- header.php
+++ header.php
@@ -6,5 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php wp_head(); ?>
     <title>Awesome theme</title>
+
+    <?php   #detects the page where the posts are displayed
+        #if(is_home()):
+        ?>
+    <?php if(is_front_page()):
+        $awesomeclasses = array('awesome-class','my-class'); 
+    else:
+    $awesomeclasses=array('no-awesome-class');
+    endif;?>
+
 </head>
-<body>
+<body <?php body_class($awesomeclasses); ?> >
+<?php wp_nav_menu(array('theme_location'=>'Main')); ?>
